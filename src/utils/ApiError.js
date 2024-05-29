0b3a0e8829da8018b3c919fe4e9455c39cbe89d6
ezbhai <varundev@gamil.com>
2024-05-29 12:53:32 +0530

    utils code

---
+++ src/utils/ApiError.js
@@ -0,0 +1,24 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "somthing went wrong",
+        error = [],
+        stack = ""
+    ){
+        super(massage)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.error = error
+
+        if(stack){
+            this.stack = stack
+        }
+        else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {ApiError}
